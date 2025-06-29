73deff0d5516b8dc0ba20439547a1d778edaac11
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-29 04:21:49 +0000

    feat: commitlint setup

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
+import path from "path";
+import tailwindcss from "tailwindcss";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     allowedHosts: true,
   },
